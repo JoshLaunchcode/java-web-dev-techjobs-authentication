c4c98f29eeb7a9ca1f80052c5d80e4b20c469f73
JoshLaunchcode <holo.sf0@gmail.com>
2021-11-08 20:11:58 -0600

    Finished studio work!

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
@@ -0,0 +1,44 @@
+package org.launchcode.javawebdevtechjobsauthentication.models;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User extends AbstractEntity{
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    @NotBlank
+    @NotNull
+    private static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public User(){}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwHash() {
+        return pwHash;
+    }
+
+    public boolean isMatchingPassword(String password){
+        return encoder.matches(password, pwHash);
+    }
+}
